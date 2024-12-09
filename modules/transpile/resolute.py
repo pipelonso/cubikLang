fb9fd876a01bcd03ee9cbfcffde63db60bb50629
pipelonso <andresfelipeibanecuta2@gmail.com>
2024-12-08 20:24:15 -0500

    ya traduce declaración + asignación de scores

--- modules/transpile/resolute.py
+++ modules/transpile/resolute.py
@@ -3,15 +3,17 @@ import os
 from modules.constans.constants import CONF_FILE, FUNCT_EXT
 from modules.system.file_system import FileSystem
 import json
+from modules.transpile.analyzer import Analyzer
 
 
 class Resolute:
 
     def __init__(self,
                  path: str = ''):
-
+        sys.tracebacklimit = 5
         self.file_system = FileSystem()
         self.function_path = ''
+        self.provided_path = ''
 
         if path.strip() != '':
             self.resolve(path)
@@ -21,6 +23,7 @@ class Resolute:
 
     def resolve(self, path):
         if os.path.exists(path):
+            self.provided_path = path
             if os.path.exists(os.path.join(path, CONF_FILE)):
                 content = self.file_system.read_file(os.path.join(path, CONF_FILE))
                 if content.strip() != '':
@@ -30,25 +33,21 @@ class Resolute:
                             print(f'Project name: {keys["name"]}')
                         if 'version' in keys:
                             print(f'Version: {keys["version"]}')
-
+                        print(1)
                         resources_exits = False
                         if 'has_resources' in keys and 'resources' in keys:
                             resources_exits = True
                             print(f'Resources folder provided: {keys["resources"]}')
-
                             if keys['has_resources']:
                                 if keys['resources'].startswith('/'):
                                     keys['resources'] = keys['resources'][1:]
-
                                 if not os.path.exists(os.path.join(path, keys["resources"])):
                                     raise Exception(f"Resources path not found, Full path:"
                                                     f" { os.path.join(path, keys['resources']) } {path}")
                                     pass
-
                         if 'functions' in keys:
                             if keys['functions'].startswith('/'):
                                 keys['functions'] = keys['functions'][1:]
-
                             if not os.path.exists(os.path.join(path, keys["functions"])):
                                 raise Exception(f"functions path not found, Full path:"
                                                 f" {os.path.join(path, keys['functions'])} {path}")
@@ -58,7 +57,6 @@ class Resolute:
                         else:
                             raise Exception(f"No functions path provided")
 
-                        print(keys)
                     except Exception as e:
                         raise Exception(f"File {CONF_FILE} Error while reading: {e}")
                     pass
@@ -70,10 +68,25 @@ class Resolute:
             pass
         else:
             raise Exception(f"Provided directory not found {path}")
+
+        self.analyze_function_trace()
+
         pass
 
-    def analyze_file_ramification(self):
+    def analyze_function_trace(self):
 
+        initial_files = self.file_system.list_files(os.path.join(self.provided_path, self.function_path))
+        for file in initial_files:
 
+            file_content = self.file_system.read_file(os.path.join(
+                    self.provided_path,
+                    self.function_path,
+                    file
+                ))
+
+            analyzer = Analyzer(file_content, file)
+            analyzer.analyze_content()
+
+            pass
 
         pass
